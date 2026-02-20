c009d96dd5b9777c6c13289a825435d990b2c575
nadav5199 <nadavpassal@gmail.com>
2026-02-20 13:24:15 +0200

    added cors

--- task-manager/backend/server.js
+++ task-manager/backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const tasksRoutes = require('./routes/tasks');
 
+app.use(cors({
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());  
 
 app.use('/api/tasks', tasksRoutes);
